87c6536fa95f5ce91936191179b34d45b6429e0e
Tusarsamal <144653655+TusaramaniSamal@users.noreply.github.com>
2023-09-16 21:52:08 +0530

    Update test_case_1.py

--- test_case_1.py
+++ test_case_1.py
@@ -12,14 +12,18 @@ def browser():
 def test_search_for_flights(browser):
     browser.get("https://www.booking.com")
     source_city = browser.find_element_by_name("ss")
-    source_city.send_keys("New York")
     destination_city = browser.find_element_by_name("ss")
+    departure_date = browser.find_element_by_css_selector(".xp__input-group .--checkin-field")
+    search_button = browser.find_element_by_css_selector(".sb-searchbox__button")
+    source_city_input = "New York"
+    destination_option_index = 1 
+    departure_date_input = "2023-09-20"
+    source_city.send_keys(source_city_input)
     destination_city.send_keys(Keys.DOWN)
     destination_city.send_keys(Keys.ENTER)
-    departure_date = browser.find_element_by_css_selector(".xp__input-group .--checkin-field")
     departure_date.clear()
-    departure_date.send_keys("2023-09-20")
-    search_button = browser.find_element_by_css_selector(".sb-searchbox__button")
+    departure_date.send_keys(departure_date_input)
     search_button.click()
-    time.sleep(5)  
+    
+    time.sleep(5)
     assert "Search Results" in browser.page_source
